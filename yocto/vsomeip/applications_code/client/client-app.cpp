c046318c33fff38ce0ac7458e6d1c8837b62a4fa
ahmed_hussien <a.hussien.91@gmail.com>
2023-04-07 16:30:36 +0200

    update someip applications

--- yocto/vsomeip/applications_code/client/client-app.cpp
+++ yocto/vsomeip/applications_code/client/client-app.cpp
@@ -1,17 +1,65 @@
 #include <iomanip>
 #include <iostream>
+#include <sstream>
+
+#include <condition_variable>
+#include <thread>
 
 #include <vsomeip/vsomeip.hpp>
 
 #define SAMPLE_SERVICE_ID 0x1234
 #define SAMPLE_INSTANCE_ID 0x5678
+#define SAMPLE_METHOD_ID 0x0421
 
 std::shared_ptr< vsomeip::application > app;
+std::mutex mutex;
+std::condition_variable condition;
+
+void run() {
+  std::unique_lock<std::mutex> its_lock(mutex);
+  condition.wait(its_lock);
+
+  std::shared_ptr< vsomeip::message > request;
+  request = vsomeip::runtime::get()->create_request();
+  request->set_service(SAMPLE_SERVICE_ID);
+  request->set_instance(SAMPLE_INSTANCE_ID);
+  request->set_method(SAMPLE_METHOD_ID);
+
+  std::shared_ptr< vsomeip::payload > its_payload = vsomeip::runtime::get()->create_payload();
+  std::vector< vsomeip::byte_t > its_payload_data;
+  for (vsomeip::byte_t i=0; i<10; i++) {
+      its_payload_data.push_back(i % 256);
+  }
+  its_payload->set_data(its_payload_data);
+  request->set_payload(its_payload);
+  app->send(request, true);
+}
+
+void on_message(const std::shared_ptr<vsomeip::message> &_response) {
+
+  std::shared_ptr<vsomeip::payload> its_payload = _response->get_payload();
+  vsomeip::length_t l = its_payload->get_length();
+
+  // Get payload
+  std::stringstream ss;
+  for (vsomeip::length_t i=0; i<l; i++) {
+     ss << std::setw(2) << std::setfill('0') << std::hex
+        << (int)*(its_payload->get_data()+i) << " ";
+  }
+
+  std::cout << "CLIENT: Received message with Client/Session ["
+      << std::setw(4) << std::setfill('0') << std::hex << _response->get_client() << "/"
+      << std::setw(4) << std::setfill('0') << std::hex << _response->get_session() << "] "
+      << ss.str() << std::endl;
+}
 
 void on_availability(vsomeip::service_t _service, vsomeip::instance_t _instance, bool _is_available) {
-    std::cout << "Service ["
+    std::cout << "CLIENT: Service ["
             << std::setw(4) << std::setfill('0') << std::hex << _service << "." << _instance
-            << "] is " << (_is_available ? "available." : "NOT available.")  << std::endl;
+            << "] is "
+            << (_is_available ? "available." : "NOT available.")
+            << std::endl;
+    condition.notify_one();
 }
 
 int main() {
@@ -20,5 +68,7 @@ int main() {
     app->init();
     app->register_availability_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, on_availability);
     app->request_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
+    app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_message);
+    std::thread sender(run);
     app->start();
 }
--- yocto/vsomeip/applications_code/server/server-app.cpp
+++ yocto/vsomeip/applications_code/server/server-app.cpp
@@ -1,14 +1,49 @@
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
 #include <vsomeip/vsomeip.hpp>
 
 #define SAMPLE_SERVICE_ID 0x1234
 #define SAMPLE_INSTANCE_ID 0x5678
+#define SAMPLE_METHOD_ID 0x0421
 
-std::shared_ptr< vsomeip::application > app;
+std::shared_ptr<vsomeip::application> app;
+
+void on_message(const std::shared_ptr<vsomeip::message> &_request) {
+
+    std::shared_ptr<vsomeip::payload> its_payload = _request->get_payload();
+    vsomeip::length_t l = its_payload->get_length();
+
+    // Get payload
+    std::stringstream ss;
+    for (vsomeip::length_t i=0; i<l; i++) {
+       ss << std::setw(2) << std::setfill('0') << std::hex
+          << (int)*(its_payload->get_data()+i) << " ";
+    }
+
+    std::cout << "SERVICE: Received message with Client/Session ["
+        << std::setw(4) << std::setfill('0') << std::hex << _request->get_client() << "/"
+        << std::setw(4) << std::setfill('0') << std::hex << _request->get_session() << "] "
+        << ss.str() << std::endl;
+
+    // Create response
+    std::shared_ptr<vsomeip::message> its_response = vsomeip::runtime::get()->create_response(_request);
+    its_payload = vsomeip::runtime::get()->create_payload();
+    std::vector<vsomeip::byte_t> its_payload_data;
+    for (int i=9; i>=0; i--) {
+        its_payload_data.push_back(i % 256);
+    }
+    its_payload->set_data(its_payload_data);
+    its_response->set_payload(its_payload);
+    app->send(its_response, true);
+}
 
 int main() {
 
-    app = vsomeip::runtime::get()->create_application("World");
-    app->init();
-    app->offer_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
-    app->start();
+   app = vsomeip::runtime::get()->create_application("World");
+   app->init();
+   app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_message);
+   app->offer_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
+   app->start();
 }
